f939dc1acf1753651dc5815bf0e05506afa2da9f
Rakesh Biswas <rbiswas01999@gmail.com>
2025-08-23 13:08:57 +0600

    C-Tiles code   -updated

--- C_Tiles.cpp
+++ C_Tiles.cpp
@@ -1,25 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MOD = 998244353;
-#define ll long long
 
-ll mod_pow(ll base, ll exp, ll mod)
-{
-    ll result = 1;
-    while (exp)
-    {
-        if (exp %2 != 0)
-        {
-            result = (result * base ) % mod;
-        }
-        base = (base * base) % mod;
-        exp = exp / 2;
+const int MOD = 998244353;
+
+// Fast exponentiation
+long long mod_pow(long long base, long long exp, long long mod) {
+    long long result = 1;
+    while (exp) {
+        if (exp & 1) result = result * base % mod;
+        base = base * base % mod;
+        exp >>= 1;
     }
     return result;
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -29,3 +24,4 @@ int main()
     cout << mod_pow(2, w + h, MOD) << '\n';
     return 0;
 }
+
